6b3e47897302554d01540d129c90658d6922722c
raviv <ravivantak@gmail.com>
2016-06-10 09:50:24 +0530

    add login credentials

--- scripts/LoginController.js
+++ scripts/LoginController.js
@@ -1,4 +1,13 @@
 angular.module("advanced-grid")
-.controller("LoginController", ["$scope", function($scope){
+.controller("LoginController", ["$scope","$location", function($scope, $location){
         $scope.name = "login page";
+        $scope.isError = false;
+        $scope.signIn = function(){
+            if($scope.email == "maheshtheapex@gmail.com" && $scope.password == "1234"){
+                $location.url("/grid");
+            } else {
+                $scope.isError = true;
+            }
+
+        };
     }]);
